26172fe3fca99eff587b208539cdf1f0f1d486fd
diserus <prozorenko24@gmail.com>
2025-12-07 16:46:47 +0700

    final eitken

--- 5/5.py
+++ 5/5.py
@@ -1,4 +1,16 @@
 from math import sqrt,factorial
+
+
+def matrixPrint(matrix):
+    max_len = max(len(f"{col:.4g}") for row in matrix for col in row)
+
+    for row in matrix:
+        for col in row:
+            val = f"{col:.4g}"  
+            print(f"{val:>{max_len+2}}", end="")
+        print()
+    print()
+
 def lagrange(x,y,point):
     n = len(x)
     result = 0
@@ -12,28 +24,23 @@ def lagrange(x,y,point):
         result+=y[i] *(chisl/znam)
     return result
 
-
-def eitken(x,y,point):
+def eitken(x, y, point):
     n = len(x)
-    P = [[0 for _ in range(n)] for _ in range(n)]
+    P = [[0] * n for _ in range(n)]
+    
+    for i in range(n):
+        P[i][0] = y[i]
+    
+    for j in range(1,n):
+        for i in range(j,n):
+             P[i][j] = ((point - x[i-j]) * P[i][j-1] - (point - x[i]) * P[i-1][j-1]) / (x[i] - x[i-j])
+    return P[n-1][-1],P
 
-    for i in range(n-1):
-        P[0][i] = (y[i]*(point - x[i+1]) - y[i+1]*(point-x[i])) / (x[i]-x[i+1])
-    for i in range(1,n-1):
-        for j in range(n-i-1):
-            P[i][j] = (P[i-1][j]*(point-x[j+i+1])-P[i-1][j+1]*(point-x[j])) / (x[j]-x[j+i+1])
-    result=P[n-2][0]
-    print("\nТаблица Эйткена:\n")
-    for i in range(n-1):
-        for j in range(n-1):
-            print(f"{P[i][j]:.6g}", end="\t")
-        print()
-    return result
 def pogreshnost(point):
     m4 = 15/16
     koef = abs((point-1)*(point-2)*(point-3)*(point-4))
     eps_usech = (m4/factorial(4))*koef
-    eps_okr = 10**(-3)
+    eps_okr = 10**(-5)
     eps_real = eps_okr + eps_usech
     return eps_real
 
@@ -47,9 +54,10 @@ if __name__=='__main__':
     print(f"\nИнтерполяция по формуле Лагранжа для x = {point}")
     print(f"Pn({point}) = {interpol_lagrange}")
 
-    interpol_eitken = eitken(x,y,point)
+    interpol_eitken,P = eitken(x,y,point)
     print(f"\nИнтерполяция по формуле Эйткена для x = {point}")
     print(f"Pn({point}) = {interpol_eitken}")
+    matrixPrint(P)
     print(f"\nСравнение в точке x = {point}\n")
     print(f"Лагранж: {interpol_lagrange} (Разница: {abs(interpol_lagrange)-sqrt(point)})")
     print(f"Эйткен: {interpol_eitken} (Разница: {abs(interpol_eitken)-sqrt(point)})")
